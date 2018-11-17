b83264d85f05236d911180d0b67c6174e2c2b47a
salad1987 <qianxunde1987@163.com>
2018-11-17 21:36:38 +0800

    更新图标 收藏点赞

--- leslie/pages/article-detail/index.js
+++ leslie/pages/article-detail/index.js
@@ -9,6 +9,7 @@ Page({
    */
   data: {
     id: 0,
+    bgxywz: null,
     title: "",
     curName: ""
   },
@@ -52,24 +53,26 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
     var that = this;
+    var user_id = wx.getStorageSync('user_id');
+    wx.showLoading();
     // 获取文章
     wx.request({
-      url: app.globalData.subDomain + '/article_detail',
+      url: app.globalData.subDomain + '/article_detail_user',
       method: 'GET',
       data: {
-        id: that.data.id
+        id: that.data.id,
+        user_id: user_id
       },
       header: {
         'Accept': 'application/json'
       },
       success: function(res) {
-        wx.hideLoading();
         if (res.data.code == 0) {
           var datas = res.data.data;
           datas.published_time = that.dataCode(datas.published_time);
           that.data.title = datas.post_title;
+          that.data.bgxywz = datas;
           that.setData({
             bgxywz: datas
           });
@@ -77,6 +80,7 @@ Page({
             datas.post_content = "小编努力整理中(ง •_•)ง 敬请期待~~ <br> 欢迎点击右下方投稿(*^__^*) ";
           }
           WxParse.wxParse('article', 'html', datas.post_content, that, 5);
+          wx.hideLoading();
         }
       }
     })
@@ -92,9 +96,95 @@ Page({
     wx.navigateBack({})
   },
 
-  toMorePage: function () {
+  toMorePage: function() {
     wx.switchTab({
       url: '/pages/first-nav/index'
+    })
+  },
+
+  toLike: function() {
+    var that = this;
+    if (!this.data.bgxywz) {
+      return;
+    }
+    var user_id = wx.getStorageSync('user_id');
+    var bgxywz = this.data.bgxywz;
+    var if_like = bgxywz.if_like;
+    wx.request({
+      url: app.globalData.subDomain + '/article_like',
+      data: {
+        id: that.data.id,
+        if_like: if_like,
+        user_id: user_id
+      },
+      success: function(res) {
+        if (if_like) {
+          bgxywz.if_like = 0;
+          bgxywz.post_like -= 1;
+          that.setData({
+            bgxywz: bgxywz
+          });
+          wx.showToast({
+            title: '取消点赞成功',
+            icon: 'none',
+            duration: 2000
+          })
+        } else {
+          bgxywz.if_like = 1;
+          bgxywz.post_like += 1;
+          that.setData({
+            bgxywz: bgxywz
+          });
+          wx.showToast({
+            title: '点赞成功',
+            icon: 'none',
+            duration: 2000
+          })
+        }
+      }
+    })
+  },
+
+  toCollect: function() {
+    var that = this;
+    if (!this.data.bgxywz) {
+      return;
+    }
+    var user_id = wx.getStorageSync('user_id');
+    var bgxywz = this.data.bgxywz;
+    var if_collect = bgxywz.if_collect;
+    wx.request({
+      url: app.globalData.subDomain + '/article_collect',
+      data: {
+        id: that.data.id,
+        if_collect: if_collect,
+        user_id: user_id
+      },
+      success: function(res) {
+        if (if_collect) {
+          bgxywz.if_collect = 0;
+          bgxywz.post_collect -= 1;
+          that.setData({
+            bgxywz: bgxywz
+          });
+          wx.showToast({
+            title: '取消收藏成功',
+            icon: 'none',
+            duration: 2000
+          })
+        } else {
+          bgxywz.if_collect = 1;
+          bgxywz.post_collect += 1;
+          that.setData({
+            bgxywz: bgxywz
+          });
+          wx.showToast({
+            title: '收藏成功',
+            icon: 'none',
+            duration: 2000
+          })
+        }
+      }
     })
   },
 
@@ -131,7 +221,7 @@ Page({
    */
   onShareAppMessage: function() {
     return {
-      title: this.data.title,
+      title: this.data.curName, //this.data.title,
       path: "/pages/article-detail/index?id=" + this.data.id + "&curName=" + this.data.curName,
       success: function(res) {
         // 转发成功
