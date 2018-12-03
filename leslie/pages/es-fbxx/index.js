a93d98ae074da0cd968f303b82f3e9aa229bb60f
salad1987 <qianxunde1987@163.com>
2018-12-03 19:47:13 +0800

    闲置信息

--- leslie/pages/es-fbxx/index.js
+++ leslie/pages/es-fbxx/index.js
@@ -6,6 +6,10 @@ Page({
    */
   data: {
     isclick: false,
+    id: 0,
+    goods_thumb: "",
+    imgs: "",
+    count_imgs: 0,
   },
 
   /**
@@ -16,6 +20,7 @@ Page({
     var id = e.id;
     if (id) {
       // 初始化原数据
+      that.data.id = id;
       wx.showLoading();
       wx.request({
         url: app.globalData.subDomain + '/esxx_detail',
@@ -25,9 +30,11 @@ Page({
         success: function (res) {
           wx.hideLoading();
           if (res.data.code == 0) {
+            var datas = res.data.data;
+            var goods_imgs = datas.goods_img.split(',');
             that.setData({
-              id: id,
-              esxxData: res.data.data
+              esxxData: res.data.data,
+              goods_imgs: goods_imgs
             });
             return;
           }
@@ -88,6 +95,92 @@ Page({
   fabuClick: function() {
     this.setData({
       isClick: true
+    })
+  },
+
+  //上传
+  clickUp: function (e) {
+    var that = this;
+    var id = that.data.id;
+    var user_id = wx.getStorageSync('user_id');
+    wx.chooseImage({
+      count: 9, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        var tempFilePaths = res.tempFilePaths;
+        // console.log(tempFilePaths);
+        // return;
+        var imgPath = tempFilePaths;
+        that.data.goods_thumb = "";
+        that.data.imgs = "";
+        that.data.count_imgs = 0;
+        for (var i = 0; i < imgPath.length; i++) {
+          wx.uploadFile({
+            url: app.globalData.subDomain + '/up_xuanyao',
+            filePath: imgPath[i] + "",
+            name: 'file',
+            header: {
+              'Accept': 'application/json'
+            },
+            success: function (res) {
+              var resData = JSON.parse(res.data.trim())
+              if (resData.code == 0) {
+                that.data.count_imgs++;
+                if (that.data.count_imgs == 1) {
+                  that.data.goods_thumb = resData.data
+                  that.data.imgs += resData.data;
+                } else {
+                  that.data.imgs += "," + resData.data;
+                }
+                if (that.data.count_imgs >= imgPath.length) {
+                  // console.log(that.data.imgs);
+                  var goods_imgs = that.data.imgs;
+                  var goods_thumb = that.data.goods_thumb;
+                  wx.request({
+                    url: app.globalData.subDomain + '/up_xy_imgs',
+                    method: 'POST',
+                    data: {
+                      id: id,
+                      user_id: user_id,
+                      goods_imgs: goods_imgs,
+                      goods_thumb: goods_thumb,
+                    },
+                    success: function (res) {
+                      wx.hideLoading();
+                      if (res.data.code == 0) {
+                        // that.setData({
+                        //   esxxData: res.data.data
+                        // });
+                        that.data.id = res.data.data;
+                        var goods_imgs = that.data.imgs.split(',');
+                        that.setData({
+                          goods_imgs: goods_imgs
+                        });
+                        wx.showToast({
+                          title: '上传成功',
+                        })
+                        return;
+                      }
+                    }
+                  })
+                }
+                // that.data.id = resData.data;
+                // wx.showToast({
+                //   title: '上传成功',
+                // })
+              } else {
+                that.data.count_imgs++;
+                if (that.data.count_imgs >= imgPath.length) { }
+                wx.showModal({
+                  content: resData.message,
+                  success: function (res) { }
+                })
+              }
+            }
+          })
+        }
+      }
     })
   },
 
